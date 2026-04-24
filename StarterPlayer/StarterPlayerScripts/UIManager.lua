70790dd8fa35dacfcaaa35152ff9b508d2fd6638
Noii <103219488+NoiiLive@users.noreply.github.com>
2026-04-24 10:55:44 -0700

    Updated StarterPlayer/StarterPlayerScripts/UIManager.lua

--- StarterPlayer/StarterPlayerScripts/UIManager.lua
+++ StarterPlayer/StarterPlayerScripts/UIManager.lua
@@ -10,14 +10,22 @@ function UIManager.Initialize()
 	local ServerListTab = require(script:WaitForChild("ServerListTab"))
 	local IndexTab = require(script:WaitForChild("IndexTab"))
 	local RecruitmentTab = require(script:WaitForChild("RecruitmentTab"))
+	local CampaignTab = require(script:WaitForChild("CampaignTab"))
+	local TeamTab = require(script:WaitForChild("TeamTab"))
+	local BattleTab = require(script:WaitForChild("BattleTab"))
 
 	local mainGui = MainTab.Create(playerGui)
 	local indexGui = IndexTab.Create(playerGui, mainGui)
 	local recruitmentGui = RecruitmentTab.Create(playerGui, mainGui)
 	local serverSelectGui = ServerListTab.Create(playerGui, mainGui)
+	local campaignGui = CampaignTab.Create(playerGui, mainGui)
+	local teamGui = TeamTab.Create(playerGui, mainGui)
+	local battleGui = BattleTab.Create(playerGui, mainGui)
 
 	local indexButton = mainGui:FindFirstChild("IndexButton", true)
 	local recruitmentButton = mainGui:FindFirstChild("RecruitmentButton", true)
+	local campaignButton = mainGui:FindFirstChild("CampaignButton", true)
+	local teamButton = mainGui:FindFirstChild("TeamButton", true)
 
 	if indexButton then
 		indexButton.MouseButton1Click:Connect(function()
@@ -32,6 +40,22 @@ function UIManager.Initialize()
 			mainGui.Enabled = false
 			RecruitmentTab.InitDefaultSelection(recruitmentGui)
 			recruitmentGui.Enabled = true
+		end)
+	end
+
+	if campaignButton then
+		campaignButton.MouseButton1Click:Connect(function()
+			mainGui.Enabled = false
+			CampaignTab.Refresh(campaignGui)
+			campaignGui.Enabled = true
+		end)
+	end
+
+	if teamButton then
+		teamButton.MouseButton1Click:Connect(function()
+			mainGui.Enabled = false
+			TeamTab.Refresh(teamGui)
+			teamGui.Enabled = true
 		end)
 	end
 end
