e057f5089a5ab1d72b78ecdb952a1864d424aee2
Noii <103219488+NoiiLive@users.noreply.github.com>
2026-04-24 10:55:57 -0700

    Updated ServerScriptService/ServerMain.lua

--- ServerScriptService/ServerMain.lua
+++ ServerScriptService/ServerMain.lua
@@ -1,4 +1,10 @@
 -- @ScriptType: Script
 local PlayerDataManager = require(script.Parent:WaitForChild("PlayerDataManager"))
+local TeamManager = require(script.Parent:WaitForChild("TeamManager"))
+local CampaignManager = require(script.Parent:WaitForChild("CampaignManager"))
+local CombatManager = require(script.Parent:WaitForChild("CombatManager"))
 
 PlayerDataManager.Initialize()
+TeamManager.Initialize()
+CampaignManager.Initialize()
+CombatManager.Initialize()
